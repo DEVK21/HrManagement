9cddf5654438ed88e14e1f96d346b4b72b65d7a6
DEVK21 <keya.java.dev@gmail.com>
2023-05-22 17:38:41 +0530

    employee class modified

--- src/main/java/com/kdev/hrm/entity/Employee.java
+++ src/main/java/com/kdev/hrm/entity/Employee.java
@@ -1,14 +1,14 @@
 package com.kdev.hrm.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-
 
 @Entity
 @Getter
